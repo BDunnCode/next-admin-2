ae46892a812be4db8bb6cb4f0df3c086258d474b
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-21 22:23:59 -0500

    next commit

--- app/ui/dashboard/header/header.jsx
+++ app/ui/dashboard/header/header.jsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import styles from "./header.module.css";
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from "react-icons/md"
 
 const Header = () => {
+
+  const pathname = usePathname();
+
+
   return (
-    <div>Header</div>
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
